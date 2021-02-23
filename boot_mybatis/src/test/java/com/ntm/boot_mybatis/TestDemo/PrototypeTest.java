6f56266620623053185ae03b8585b8aaf7d35458
WH1909030 <Scott_Niu@wistronits.com>
2021-02-23 09:31:03 +0800

    托普利策矩阵，Integer自动拆装箱

--- boot_mybatis/src/test/java/com/ntm/boot_mybatis/TestDemo/PrototypeTest.java
+++ boot_mybatis/src/test/java/com/ntm/boot_mybatis/TestDemo/PrototypeTest.java
@@ -2,6 +2,7 @@ package com.ntm.boot_mybatis.TestDemo;
 
 import com.ntm.boot_mybatis.DesignPatterns.Citation;
 import com.ntm.boot_mybatis.DesignPatterns.SunWuKong;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.Test;
 
 import javax.swing.*;
@@ -33,5 +34,46 @@ public class PrototypeTest {
         citation2.setName("李四");
         citation2.display();
 
+    }
+
+    @Test
+    public void Test2() {
+        int[][] materil = {{1, 2, 3, 4}, {5, 1, 2, 3}, {9, 5, 1, 2}};
+        boolean p = isTuoPuLiCe(materil);
+        System.out.println(p);
+    }
+
+    //托普利策矩阵
+    public boolean isTuoPuLiCe(int[][] materil) {
+        int m = materil.length;
+        int n = materil[0].length;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (materil[i][j] != materil[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    //自动拆装箱 Integer范围（-128~127）
+    @Test
+    public void testInt() {
+        Integer i1 = new Integer(123);
+        Integer i2 = new Integer(123);
+        System.out.println(i1 == i2);//false
+
+        Integer i3 = 123;
+        System.out.println(i2 == i3);//false
+        Integer i4 = 123;
+        System.out.println(i3 == i4);//true
+
+        Integer i5 = 128;
+        Integer i6 = 128;
+        System.out.println(i5 == i6);//false
+
+        int i7 = 128;
+        System.out.println(i6 == i7);//true
     }
 }
